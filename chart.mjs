72892d60e551aa1e302cb6d1b5e456f379652393
robert <kaukana0@tuta.io>
2022-06-13 18:00:29 +0200

    missing data notice and legend css

--- chart.mjs
+++ chart.mjs
@@ -5,6 +5,70 @@ export let chart
 let unitText  // maybe put in some kind of closure?
 let isInitialized = false
 let legendCurrentSelection
+let uniquePrefix  // a crook because of light-DOM to avoid problems w/ multiple charts
+let toast
+let dataMissing = []
+
+
+export function init(type, chartCSSSelector, legendCSSSelector, cols, bla, categories, unit) {
+  unitText = unit
+  
+  if(isInitialized) {
+    update(cols)
+  } else {
+    isInitialized = true
+    uniquePrefix = "chartElement" + Math.floor(Math.random() * 10000)
+
+    document.body.insertAdjacentHTML("beforeend", toastHtml())
+    toast = new bootstrap.Toast(document.getElementById(uniquePrefix+"toast"))
+
+    chart = bb.generate({
+      bindto: chartCSSSelector,
+      data: {
+        columns: cols,
+        type: type,
+        onshown: function(ids) {  // NOT WORKING... WHY?
+          console.log("H")
+        }
+      },
+      grid: grid(),
+      axis: axis(categories),
+      tooltip: {show:true,
+        format: {
+          name: function(name, ratio, id, index) { return bla.get(id) },
+          value: function(value, ratio, id, index) { return value + unitText }
+        }
+      },
+      legend: legend(legendCSSSelector)
+    })
+  }
+}
+
+
+export function update(cols) {
+	chart.load({
+    unload: true, // TODO - use the diff to make smooth transition
+		columns: cols
+  })
+  window.requestAnimationFrame(e=>coloringForMissingData(cols))
+}
+
+
+function toastHtml() {
+return `
+<div class="toast-container position-absolute top-50 start-50 translate-middle" style="z-index: 50;">
+  <div id="${uniquePrefix}toast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      <strong class="me-auto">Info</strong>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">
+      No data available for your selection.
+      Please change your selection.
+    </div>
+  </div>
+</div>
+`}
 
 
 function legend(legendCSSSelector) {
@@ -15,7 +79,7 @@ function legend(legendCSSSelector) {
 
     /* > bootstrap xs  legend is supposed to be on the right of chart */
     @media screen and (min-width: 576px) {
-      #chartLegend {
+      .${uniquePrefix} {
         display: block;
         border-left: 25px solid;
         padding-top: 0.7em; 
@@ -27,7 +91,7 @@ function legend(legendCSSSelector) {
 
     /* < bootstrap xs   legend is supposed to be below chart */
     @media (max-width: 576px) {
-      #chartLegend {
+      .${uniquePrefix} {
         display: inline-block;
         margin-top: 20px;
         border-top: 8px solid;
@@ -47,14 +111,14 @@ function legend(legendCSSSelector) {
     `
   }
 
-  document.head.insertAdjacentHTML("beforeend", style())
+  document.head.insertAdjacentHTML("beforeend", style())    // :-/  do that not here
 
   return {
     position: "right",
     contents: {
       bindto: legendCSSSelector,
       template: function(title, color) {
-        return `<span id="chartLegend" style="border-color: ${color};">${title}</span>`
+        return `<span class="${uniquePrefix}" id="${uniquePrefix+title}" style="border-color: ${color}; ${dataMissing.includes(title)?"text-decoration: line-through;":""}">${title}</span>`
       }
     },
     item: {
@@ -79,8 +143,8 @@ function legend(legendCSSSelector) {
        onout: function(id) {},
     }
   }
-
 }
+
 
 function grid() {
   return {
@@ -90,6 +154,7 @@ function grid() {
     }
   }
 }
+
 
 function axis(categories) {
   return {
@@ -109,35 +174,26 @@ function axis(categories) {
   }
 }
 
-export function init(type, chartCSSSelector, legendCSSSelector, cols, bla, categories, unit) {
-    unitText = unit
-    if(isInitialized) {
-      update(cols)
-    } else {
-      isInitialized = true
 
-      chart = bb.generate({
-        bindto: chartCSSSelector,
-        data: {
-          columns: cols,
-          type: type,
-        },
-        grid: grid(),
-        axis: axis(categories),
-        tooltip: {show:true,
-        format: {
-          name: function(name, ratio, id, index) { return bla.get(id) },
-          value: function(value, ratio, id, index) { return value + unitText }
-        }
-        },
-        legend: legend(legendCSSSelector)
-      });
+function coloringForMissingData(cols) {
+  dataMissing = []
+
+  cols.forEach(col => {
+    if(col.slice(1).every(el => el===null)) {
+      dataMissing.push(col[0])
     }
-}
- 
-export function update(cols) {
-	chart.load({
-    unload: true, // TODO - use the diff to make smooth transition
-		columns: cols
-	})
+  })
+
+  if(cols.length === dataMissing.length) {
+    toast.show()
+  }
+
+  dataMissing.forEach(el => {
+    let o = {}
+    o[el] = "#999"
+    chart.data.colors(o)
+    // window.setTimeout( e => {
+    //   document.getElementById(uniquePrefix+el).setAttribute("style","text-decoration: line-through;")
+    // } , 500);    
+  })    
 }
