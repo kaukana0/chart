789f45b88a92197d37b940b23d626ba9f4eea196
robert <kaukana0@tuta.io>
2022-06-15 13:31:33 +0200

    make toast more noticeable

--- chart.mjs
+++ chart.mjs
@@ -60,14 +60,14 @@ export function setYLabel(text) {
 
 function toastHtml() {
 return `
-<div class="toast-container position-absolute top-50 start-50 translate-middle" style="z-index: 50; background-color: #fff;">
-  <div id="${uniquePrefix}toast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+<div class="toast-container position-fixed top-50 start-50 translate-middle" style="z-index: 50; background-color: #fff;">
+  <div id="${uniquePrefix}toast" class="toast text-white bg-primary" role="alert" aria-live="assertive" aria-atomic="true">
     <div class="toast-header">
       <strong class="me-auto">Info</strong>
       <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
     </div>
-    <div class="toast-body">
-      No data available for your selection.
+    <div class="toast-body text-center">
+      No data available for your selection.<br>
       Please change your selection.
     </div>
   </div>
