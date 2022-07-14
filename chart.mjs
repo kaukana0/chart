8e4852273d1b06ef738770d8418c1e35979a7ce8
robert <kaukana0@tuta.io>
2022-07-14 17:21:26 +0200

    rework for multiselection (WIP)

--- chart.mjs
+++ chart.mjs
@@ -3,7 +3,7 @@ import toastHtml from "./toast.mjs"
 import {grid, axis} from "./rest.mjs"
 
 
-class States {
+class State {
 	static states = new Map()
 
 	static add(chartDOMElementId, legendDOMElementId, bla, categories, unitText) {
@@ -19,61 +19,34 @@ class States {
 		})
 		return this.get(chartDOMElementId)
 	}
+
+	static update(id, categories, unitText) {
+		const state = this.get(id)
+		if(state) {
+			state.categories = categories
+			state.unitText = unitText
+		}
+		return state
+	}
 	
 	static has(id) { return this.states.has(id) }
 	static get(id) { return this.states.get(id) }
 }
-let states = new States()
 
 let toast
 
 
-
 export function init(type, chartDOMElementId, legendDOMElementId, cols, bla, categories, unitText) {
-	if(States.has(chartDOMElementId)) {
-		update(cols, States.get(chartDOMElementId))
+	if(State.has(chartDOMElementId)) {
+		updateChart(cols, State.update(chartDOMElementId, categories, unitText))
 	} else {
 		// create a new state, a new billoardjs-chart and hook up a legend to the chart
-		connectLegend(createChart(States.add(chartDOMElementId, legendDOMElementId, bla, categories, unitText), type))
-		update(cols, States.get(chartDOMElementId))
+		connectLegend(createChart(State.add(chartDOMElementId, legendDOMElementId, bla, categories, unitText), type))
+		updateChart(cols, State.get(chartDOMElementId))
 		if(!toast) {
-			toast = createToast(States.get(chartDOMElementId).uniquePrefix)	// any uniquePrefix would do really; just take from 1st chart out of convenience
+			toast = createToast(State.get(chartDOMElementId).uniquePrefix)	// any uniquePrefix would do really; just take from 1st chart out of convenience
 		}
 	}
-}
-
-export function update(cols, chart) {
-	chart.chart.load({
-		unload: getDiff(chart.currentCols, cols), 	// smooth transition
-		columns: cols,
-		done: function () {
-			if(!displayMissingDataInLegend(cols, chart.uniquePrefix)) {
-				toast.show()	// disappears by itself
-			}
-			//addLegendKeyboardNavigability(chart.legendDOMElementId)
-		}
-	})
-	chart.currentCols = cols
-}
-
-export function setYLabel(chartDOMElementId, text) {
-	States.get(chartDOMElementId).chart.axis.labels({ y: text })
-}
-
-// which of currentCols are not in newCols? returns array.
-function getDiff(currentCols, newCols) {
-	let retVal = []
-	currentCols.forEach(e=> {
-		if( newCols.filter(e2=>e2[0]==e[0]).length == 0 ) {
-			retVal.push(e[0])
-		}
-	})
-	return retVal
-}
-
-function createToast(uniquePrefix) {
-	document.body.insertAdjacentHTML("beforeend", toastHtml(uniquePrefix + "toast"))
-	return new bootstrap.Toast(document.getElementById(uniquePrefix + "toast"))
 }
 
 function createChart(chartState, type) {
@@ -95,6 +68,43 @@ function createChart(chartState, type) {
 		legend: legend(chartState.legendDOMElementId, chartState.uniquePrefix)
 	})
 	return chartState
+}
+
+export function updateChart(cols, chart) {
+	console.log(chart.categories)
+	chart.chart.load({
+		unload: getDiff(chart.currentCols, cols), 	// smooth transition
+		columns: cols,
+		categories: chart.categories,
+		//axes: axis(chartState.categories),
+		done: function () {
+			if(!displayMissingDataInLegend(cols, chart.uniquePrefix)) {
+				toast.show()	// disappears by itself
+			}
+			//addLegendKeyboardNavigability(chart.legendDOMElementId)
+		}
+	})
+	chart.currentCols = cols
+}
+
+export function setYLabel(chartDOMElementId, text) {
+	State.get(chartDOMElementId).chart.axis.labels({ y: text })
+}
+
+// which of currentCols are not in newCols? returns array.
+function getDiff(currentCols, newCols) {
+	let retVal = []
+	currentCols.forEach(e=> {
+		if( newCols.filter(e2=>e2[0]==e[0]).length == 0 ) {
+			retVal.push(e[0])
+		}
+	})
+	return retVal
+}
+
+function createToast(uniquePrefix) {
+	document.body.insertAdjacentHTML("beforeend", toastHtml(uniquePrefix + "toast"))
+	return new bootstrap.Toast(document.getElementById(uniquePrefix + "toast"))
 }
 
 function connectLegend(chartState) {
